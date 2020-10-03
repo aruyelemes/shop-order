4ad153ab5c8b900a9109ac7baa9b26ba6349b038
Aru <aru.yelemes23@gmail.com>
2020-10-03 17:04:05 +0600

    Second commit

--- src/main/java/kz/iitu/shop_order/ShopOrderApplication.java
+++ src/main/java/kz/iitu/shop_order/ShopOrderApplication.java
@@ -2,8 +2,10 @@ package kz.iitu.shop_order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class ShopOrderApplication {
 
 	public static void main(String[] args) {
